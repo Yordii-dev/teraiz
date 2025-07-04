78f051e81a35182d73132501dfcacf9de8bc9153
Yordii-dev <168145330+Yordii-dev@users.noreply.github.com>
2025-07-04 12:07:09 +0000

    fix .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: import.meta.env.VITE_BASE_PATH || '/techraiz/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Esta línea reemplaza process.cwd()
+  const env = loadEnv(mode, new URL('.', import.meta.url).pathname)
+
+  return {
+    base: env.VITE_BASE_PATH || '/techraiz/',
+    plugins: [react()],
+  }
 })
