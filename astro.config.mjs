ea6276783f5d978590ec5d3f8ec9e5177c76ec56
Yordii-dev <yokdy3602@gmail.com>
2025-08-06 14:52:51 -0500

    add: seo sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://www.teraiz.com",
+
+  integrations: [react(), sitemap()],
   vite: {
     resolve: {
       alias: {
